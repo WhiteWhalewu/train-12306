be2e281973b1aa54fd53f520e2a4d06b79d826d6
Wu ZhuiWei <550177201@qq.com>
2024-12-01 21:51:06 +0800

    1.3 增加http测试文件，方便在idea中测试

--- member/src/main/java/com/train/member/MemberApplication.java
+++ member/src/main/java/com/train/member/MemberApplication.java
@@ -17,7 +17,7 @@ public class MemberApplication {
         SpringApplication application = new SpringApplication(MemberApplication.class);
         Environment environment = application.run(args).getEnvironment();
         Log.info("启动成功");
-        Log.info("地址：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
+        Log.info("地址：\thttp://127.0.0.1:{}{}/hello",environment.getProperty("server.port"),environment.getProperty("server.servlet.context-path"));
     }
 
 }
